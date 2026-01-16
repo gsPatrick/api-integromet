29f70b82349d7fec79d7e4e30b31dbdbca56ed5c
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-01-16 17:15:55 -0300

    feat: add missing validate route

--- backend/src/server.js
+++ backend/src/server.js
@@ -601,6 +601,7 @@ app.post('/orders/generate-link-sync', orderController.generateLinkSync.bind(ord
 app.get('/orders/:id', orderController.getOrder);
 app.put('/orders/:id', orderController.updateOrder);
 app.post('/orders/:id/sync-bling', orderController.syncOrderToBling.bind(orderController));
+app.post('/orders/:id/validate', orderController.validateOrder.bind(orderController));
 app.post('/admin/fix-lili', orderController.fixLili.bind(orderController));
 
 // Start Server
